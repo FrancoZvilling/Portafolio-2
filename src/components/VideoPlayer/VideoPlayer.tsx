8cfa4988a52ecb02f2185708174731c39bfca117
Franco <francozvilling2014@hotmail.com>
2025-08-12 01:13:14 -0300

    video

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,11 +1,15 @@
-// src/components/VideoPlayer/VideoPlayer.tsx (VERSIÓN SIMPLE Y ROBUSTA)
+// src/components/VideoPlayer/VideoPlayer.tsx (VERSIÓN FINAL PARA react-player@2.12.0)
 import ReactPlayer from 'react-player';
 import styles from './VideoPlayer.module.css';
 
+// No necesitamos importar React aquí
+
+// Definimos la interfaz para las props
 interface VideoPlayerProps {
   url: string;
 }
 
+// Creamos el componente como una función simple
 function VideoPlayer({ url }: VideoPlayerProps) {
   return (
     <div className={styles.playerWrapper}>
@@ -15,7 +19,7 @@ function VideoPlayer({ url }: VideoPlayerProps) {
         width="100%"
         height="100%"
         controls={true}
-        light={true}
+        light={true} 
       />
     </div>
   );
